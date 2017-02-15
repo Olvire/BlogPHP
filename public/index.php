dcdc9df31b7f27719b92b184250829587a5f4797
Eric Rodriguez <eric.rodriguez.pp@gmail.com>
2017-02-15 16:57:47 +0100

    premier commit, création structure et autoloader

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require '../app/Autoloader.php';
+
+App\Autoloader::register();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Coucou</h1>
+</body>
+</html>
+ 
